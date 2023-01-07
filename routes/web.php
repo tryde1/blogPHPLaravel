835e55de23816d527e725448299d94d9702c795f
Alexey <ermak.leha@inbox.ru>
2023-01-07 23:36:26 +1000

    Redirect from / to /profile

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'LoginController@show')->name('login.show');
+
 Route::get('/register', 'RegisterController@show')->name('register.show');
 Route::post('/register', 'RegisterController@register')->name('register.action');
 
